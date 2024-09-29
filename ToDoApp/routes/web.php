79fe8b2dd9bc44b1b5075d628531119743173e43
ZZSpasov20 <ZZSpasov20@codingburgas.bg>
2024-09-29 17:05:37 +0300

    Add create item and mark it as complete functionalities

--- ToDoApp/routes/web.php
+++ ToDoApp/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ToDoListController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ToDoListController::class, 'index'] );
+
+Route::post('/saveItem', [ToDoListController::class, 'saveItem'])->name('saveItem');
+Route::post('/markAsComplete/{id}', [ToDoListController::class, 'markAsComplete'])->name('markAsComplete');
